60d10bea63575fad713b6a5bc584334b4f239ca8
HyeonJeong <dlaus1214@gmail.com>
2024-06-05 22:15:45 +0900

    [Feat] 프로그래스바 UI

--- Dokgi/View/HomeViewController.swift
+++ Dokgi/View/HomeViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import SnapKit
 
 class HomeViewController: UIViewController {
+    let viewModel = HomeViewModel()
+    
     private enum Const {
         static let itemSize = CGSize(width: 300, height: 164)
         static let itemSpacing = 10.0
@@ -41,8 +43,14 @@ class HomeViewController: UIViewController {
         return view
     }()
     
+    let nextLengthLabel = UILabel()
+    let lengthProgress = UIProgressView()
+    let currentPosition = UIImageView()
+    let currentPositionImage = UIImageView()
+    let nextPosition = UIImageView()
+    let nextpositionImage = UIImageView()
+    let progressPosition = UIView()
     
-    let viewModel = HomeViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,8 +61,10 @@ class HomeViewController: UIViewController {
     }
     
     func setupConstraints() {
-        view.addSubview(currentLengthLabel)
-        view.addSubview(currentLengthCollectionView)
+        [currentLengthLabel, currentLengthCollectionView, nextLengthLabel, lengthProgress, currentPosition, currentPositionImage, nextPosition, nextpositionImage, progressPosition].forEach {
+            view.addSubview($0)
+        }
+        
         currentLengthLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(112)
             $0.leading.equalToSuperview().offset(29)
@@ -66,6 +76,51 @@ class HomeViewController: UIViewController {
             $0.centerX.equalToSuperview()
             $0.height.equalTo(200)
         }
+        
+        nextLengthLabel.snp.makeConstraints {
+            $0.top.equalTo(currentLengthCollectionView.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(35)
+            $0.trailing.equalToSuperview().offset(-35)
+        }
+        
+        lengthProgress.snp.makeConstraints {
+            $0.leading.trailing.equalTo(nextLengthLabel)
+            $0.top.equalTo(nextLengthLabel.snp.bottom).offset(18)
+        }
+        
+        progressPosition.snp.makeConstraints {
+            $0.centerY.equalTo(lengthProgress.snp.centerY)
+            $0.leading.equalTo(currentPosition.snp.leading).offset(11)
+            $0.width.height.equalTo(16)
+        }
+        
+        currentPosition.snp.makeConstraints {
+            $0.width.equalTo(38)
+            $0.height.equalTo(41)
+            $0.top.equalTo(lengthProgress.snp.bottom).offset(7)
+            $0.leading.equalTo(lengthProgress.snp.leading).offset(-17)
+        }
+        
+        currentPositionImage.snp.makeConstraints {
+            $0.width.equalTo(36)
+            $0.height.equalTo(36)
+            $0.top.equalTo(currentPosition.snp.top).offset(4)
+            $0.centerX.equalTo(currentPosition.snp.centerX)
+        }
+        
+        nextPosition.snp.makeConstraints {
+            $0.width.equalTo(38)
+            $0.height.equalTo(41)
+            $0.top.equalTo(lengthProgress.snp.bottom).offset(7)
+            $0.trailing.equalTo(lengthProgress.snp.trailing).offset(17)
+        }
+        
+        nextpositionImage.snp.makeConstraints {
+            $0.width.equalTo(36)
+            $0.height.equalTo(36)
+            $0.top.equalTo(nextPosition.snp.top).offset(4)
+            $0.centerX.equalTo(nextPosition.snp.centerX)
+        }
     }
     
     func configureUI() {
@@ -73,6 +128,28 @@ class HomeViewController: UIViewController {
         
         currentLengthLabel.text = "현재 구절 길이"
         currentLengthLabel.font = .systemFont(ofSize: 20, weight: .semibold)
+        
+        nextLengthLabel.text = "다음까지 남은 길이는?"
+        nextLengthLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        
+        lengthProgress.progressViewStyle = .default
+        lengthProgress.progressTintColor = .deepSkyBlue
+        lengthProgress.transform = lengthProgress.transform.scaledBy(x: 1, y: 2)
+        
+        progressPosition.layer.cornerRadius = 9
+        progressPosition.backgroundColor = .deepSkyBlue
+        currentPosition.image = UIImage(named: "speechBubble1")
+        nextPosition.image = UIImage(named: "speechBubble2")
+        currentPositionImage.backgroundColor = .red
+        currentPositionImage.layer.cornerRadius = 18
+        currentPositionImage.image = UIImage(named: "grape")
+        currentPositionImage.layer.masksToBounds = true
+        currentPositionImage.contentMode = .scaleAspectFit
+        nextpositionImage.backgroundColor = .yellow
+        nextpositionImage.layer.cornerRadius = 18
+        nextpositionImage.image = UIImage(named: "goni")
+        nextpositionImage.contentMode = .scaleAspectFit
+        nextpositionImage.layer.masksToBounds = true
     }
 
 }
