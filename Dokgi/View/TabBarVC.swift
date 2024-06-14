f08a726d6b3fa3ea682d11bb47242f599a2416ca
suri0000 <yesle2005@naver.com>
2024-06-15 02:50:11 +0900

    [Feat]#59-TabBar Blur

--- Dokgi/View/TabBarVC.swift
+++ Dokgi/View/TabBarVC.swift
@@ -8,28 +8,29 @@
 import UIKit
 
 class TabBarVC: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBar.backgroundColor = .white
-        tabBar.alpha = 0.75
-
+        tabBar.backgroundColor = .white.withAlphaComponent(0.7)
+        tabBar.layer.backgroundColor = UIColor.clear.cgColor
+        setUpBlur()
+        
         // 홈화면 설정
         let homePageVC = HomeViewController()
         homePageVC.tabBarItem = UITabBarItem(title: "홈", image: .tabBarHome, tag: 0)
-
+        
         // 구절화면 설정
         let verseVC = ParagraphViewController()
         verseVC.tabBarItem = UITabBarItem(title: "구절", image: .tabBarParagraph, tag: 1)
-
+        
         // 내 서재 설정
         let myLibraryVC = LibrarySearchViewController()
         myLibraryVC.tabBarItem = UITabBarItem(title: "내 서재", image: .tabBarMyLibrary, tag: 2)
-
+        
         // 탭바 컨트롤러에 뷰 컨트롤러 설정
         let controllers = [homePageVC, verseVC, myLibraryVC]
         viewControllers = controllers.map { UINavigationController(rootViewController: $0) } // RootViewController 설정
-
+        
         // 탭 바의 색상 설정
         UITabBar.appearance().tintColor = .charcoalBlue // 선택된 아이템
         UITabBar.appearance().unselectedItemTintColor = .tabBarGray// 선택되지 않은 아이템
@@ -38,7 +39,16 @@ class TabBarVC: UITabBarController {
         let lineView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 0.5))
         lineView.backgroundColor = .tabBarGray
         tabBar.addSubview(lineView)
-        
-        tabBar.backgroundColor = .white
+    }
+    
+    private func setUpBlur() {
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = tabBar.bounds
+        blurView.autoresizingMask = .flexibleWidth
+        tabBar.insertSubview(blurView, at: 0)
+        blurView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
