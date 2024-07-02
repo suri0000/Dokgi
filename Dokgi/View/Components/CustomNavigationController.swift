1c1db321e4300017d489b48d09cf85186c78ff8f
qkwmapel <otxoid@gmail.com>
2024-07-02 17:39:49 +0900

    [Fix]#185-불필요한 주석제거

--- Dokgi/View/Components/CustomNavigationController.swift
+++ Dokgi/View/Components/CustomNavigationController.swift
@@ -5,9 +5,9 @@
 //  Created by 송정훈 on 7/2/24.
 //
 
-import UIKit
-import RxSwift
 import RxCocoa
+import RxSwift
+import UIKit
 
 class CustomNavigationController: UINavigationController {
     let disposeBag = DisposeBag()
@@ -15,7 +15,7 @@ class CustomNavigationController: UINavigationController {
     let backButton = UIButton().then {
         $0.setImage(UIImage.backicon, for: .normal)
         $0.setTitle("Back", for: .normal)
-        $0.setTitleColor(.brightBlue, for: .normal) // Set title color
+        $0.setTitleColor(.brightBlue, for: .normal)
         $0.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         $0.sizeToFit()
     }
