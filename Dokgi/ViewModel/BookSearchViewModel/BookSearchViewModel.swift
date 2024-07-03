2f0a91fc65f98b980bcad24531e295ba4ef8ed8c
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-07-03 20:46:58 +0900

    [Fix]#191-필요없는 변수 삭제

--- Dokgi/ViewModel/BookSearchViewModel/BookSearchViewModel.swift
+++ Dokgi/ViewModel/BookSearchViewModel/BookSearchViewModel.swift
@@ -11,7 +11,6 @@ class BookSearchViewModel {
     
     private let bookManager = BookManager.shared
     
-    private var lastSavedQuery: String?
     var searchResults: [Item] = []
     var isLoading = false
     var isLoadingLast = false
