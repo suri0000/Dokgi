fe29a6a902226cf0d2c0354db9e06876f729630e
suri0000 <yesle2005@naver.com>
2024-11-16 14:43:42 +0900

    [Bug]#5-Passage CollectionView Bottom AutoLayout

--- Dokgi/View/PassageView/Cell/PassageCollectionViewLayout.swift
+++ Dokgi/View/PassageView/Cell/PassageCollectionViewLayout.swift
@@ -31,17 +31,18 @@ final class PassageCollectionViewLayout: UICollectionViewFlowLayout {
         return CGSize(width: contentWidth, height: contentHeight)
     }
     
+    // 콜렉션 뷰가 처음 초기화되거나 뷰가 변경될 때 실행됩니다. 이 메서드에서 레이아웃을 미리 계산하여 메모리에 적재하고, 필요할 때마다 효율적으로 접근할 수 있도록 구현
     override func prepare() {
         guard let collectionView = collectionView, cache.isEmpty else { return }
         
-        let numberOfColumns: Int = 2
+        let numberOfColumns: Int = 2    // 열 개수
         let cellPadding: CGFloat = 6
         let cellWidth: CGFloat = contentWidth / CGFloat(numberOfColumns)
         
-        let xOffSet: [CGFloat] = [0, cellWidth]
-        var yOffSet: [CGFloat] = .init(repeating: 0, count: numberOfColumns)
+        let xOffSet: [CGFloat] = [0, cellWidth]     // 셀의 x 위치를 나타내는 배열
+        var yOffSet: [CGFloat] = .init(repeating: 0, count: numberOfColumns)    // 셀의 y 위치를 나타내는 배열
         
-        var column: Int = 0
+        var column: Int = 0 // 현재 열의 위치(첫번째 0, 두번째 1)
         
         for item in 0..<collectionView.numberOfItems(inSection: 0) {
             let indexPath = IndexPath(item: item, section: 0)
@@ -49,20 +50,27 @@ final class PassageCollectionViewLayout: UICollectionViewFlowLayout {
             let textHeight = delegate?.collectionView(collectionView, heightForTextAtIndexPath: indexPath) ?? 0
             let height = cellPadding * 2 + textHeight
             
-            let frame = CGRect(x: xOffSet[column], y: yOffSet[column], width: cellWidth, height: height)
+            let frame = CGRect(x: xOffSet[column], 
+                               y: yOffSet[column],
+                               width: cellWidth,
+                               height: height)
             let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
             
+            // 위에서 계산한 Frame 을 기반으로 cache 에 들어갈 레이아웃 정보를 추가합니다.
             let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
             attributes.frame = insetFrame
             cache.append(attributes)
             
+            // 콜렉션 뷰의 contentHeight 를 다시 지정합니다.
             contentHeight = max(contentHeight, frame.maxY)
             yOffSet[column] = yOffSet[column] + height
             
+            // 다른 이미지 크기로 인해서, 한쪽 열에만 이미지가 추가되는 것을 방지합니다.
             column = yOffSet[0] > yOffSet[1] ? 1 : 0
         }
     }
     
+    // 모든 셀과 보충 뷰의 레이아웃 정보를 리턴합니다. 화면 표시 영역 기반(Rect)의 요청이 들어올 때 사용합니다.
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         var visibleLayoutAttributes: [UICollectionViewLayoutAttributes] = []
         
@@ -75,6 +83,7 @@ final class PassageCollectionViewLayout: UICollectionViewFlowLayout {
         return visibleLayoutAttributes
     }
     
+    // 모든 셀의 레이아웃 정보를 리턴합니다. IndexPath 로 요청이 들어올 때 이 메서드를 사용합니다.
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
         return cache[indexPath.item]
     }
